d3a396923ddb4342550e229a95c1e2a6bb245b02
Jack Pults <jill.kills.jack@gmail.com>
2016-04-02 14:41:23 -0500

    Update car.js

--- js/car.js
+++ js/car.js
@@ -57,8 +57,8 @@ function sendObstacle() {
     box = {
       x: Math.floor((Math.random() * canvas.width) ),
       y: -10,
-      h: Math.floor((Math.random() * 50) + 1),
-      l: Math.floor((Math.random() * 50) + 1)
+      h: Math.floor((Math.random() * 50) + 10),
+      l: Math.floor((Math.random() * 50) + 10)
     };
     obstacles.push(box);
 }
